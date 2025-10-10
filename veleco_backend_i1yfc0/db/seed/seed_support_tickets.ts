7d2872acfb14d26903826535826bb7e9bc6bd0cf
veloceeo <veloceo69@gmail.com>
2025-10-10 13:23:25 +0530

    Update seed_support_tickets.ts

--- veleco_backend_i1yfc0/db/seed/seed_support_tickets.ts
+++ veleco_backend_i1yfc0/db/seed/seed_support_tickets.ts
@@ -14,7 +14,7 @@
  *   ts-node seed_support_tickets.ts
  */
 
-import { seedSupportTicketData, cleanupSupportTicketData } from './seed_support_ticket_data';
+import { seedSupportTicketData, cleanupSupportTicketData } from './seed_support_ticket_data.js';
 
 async function main() {
   const command = process.argv[2];
@@ -59,5 +59,6 @@ async function main() {
 if (require.main === module) {
   main();
 }
+
 
 export default main;
