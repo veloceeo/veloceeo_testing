03fcf63463fd6949f51faed0eaed5b1f8012589a
veloceeo <veloceo69@gmail.com>
2025-10-10 13:14:29 +0530

    Update seed_settings_data.ts

--- veleco_backend_i1yfc0/db/seed/seed_settings_data.ts
+++ veleco_backend_i1yfc0/db/seed/seed_settings_data.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from '../generated/prisma/index.js';
 
 const prisma = new PrismaClient();
 
@@ -285,4 +285,5 @@ if (require.main === module) {
 
 
 export default seedSettingsData;
+
 
